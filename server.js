c72be9365b27e0abfe627da39e5d45e99f466e45
Gaurav Nayyar <gauravnayyar8603@gmail.com>
2026-07-15 10:18:56 +0530

    All is Fine

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-    res.json({ message: "All is Wellzz" });
+    res.json({ message: "All is Wellzzz" });
 });
 
 app.listen(port, () => {
